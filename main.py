8f72001b3350a234114d102af582b22a81fb6677
MartinSec <martinsec20@proton.me>
2024-12-02 22:35:10 +0000

    csv_export and json_export functions created, basic logging created for both functions.

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ def main():
     logging.basicConfig(filename='logs/parser.log',
                         level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s')
-    pprint.pprint(parser.extract_basic_info(parser.load_pcap("data/raw/3-way-handshake.pcap")), sort_dicts=False)
-    exporter.csv_export(parser.extract_basic_info(parser.load_pcap("data/raw/200722_tcp_anon.pcapng")))
+    output = (parser.extract_basic_info(parser.load_pcap("data/raw/200722_tcp_anon.pcapng")))
+    pprint.pprint(output, sort_dicts=False)
+    exporter.csv_export(output)
+    exporter.json_export(output)
 
 if __name__ == "__main__":
     main()
--- src/exporter.py
+++ src/exporter.py
@@ -1,10 +1,15 @@
-from typing import Any
-
 import pandas as pd
 import logging
 
 def csv_export(data: dict[int, dict[str, str]]) -> None:
     data_frame = pd.DataFrame.from_dict(data, orient='index')
-    date_frame = data_frame.T
-    data_frame.to_csv("data/raw/export.csv", index=True)
+    data_frame.to_csv("./export.csv", index=True)
     logging.info("Exported data")
+
+def json_export(data: dict[int, dict[str, str]]) -> None:
+    data_frame = pd.DataFrame.from_dict(data, orient='index')
+    data_frame.reset_index(inplace=True)
+    data_frame.rename(columns={'index': 'Packet ID'}, inplace=True)
+    data_frame.to_json("./export.json", orient='records')
+    logging.info("Exported data")
+
