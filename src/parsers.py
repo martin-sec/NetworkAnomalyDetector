18b4672f7f7e8de4b465ac69ad923a2d8a44ac5d
MartinSec <martinsec20@proton.me>
2024-12-02 17:05:43 +0000

    Basic information extraction completed.

--- src/parsers.py
+++ src/parsers.py
@@ -1,6 +1,8 @@
 import pyshark
 from pyshark import FileCapture
+from pyshark.packet.packet import Packet
 
+import pprint
 
 
 def load_pcap(filepath: str) -> FileCapture | None:
@@ -10,20 +12,48 @@ def load_pcap(filepath: str) -> FileCapture | None:
     except FileNotFoundError:
         return None
 
-def extract_basic_info(capture: FileCapture) -> dict:
-    basic_pkt_info: dict[int, {str, str}] = {}
+def has_timestamp(pkt: Packet) -> bool:
+    return bool(pkt.sniff_timestamp)
+
+def has_network_layer(pkt: Packet) -> bool:
+    return bool(pkt.ip)
+
+def has_transport_layer(pkt: Packet) -> bool:
+    return bool(pkt.transport_layer)
+
+def extract_basic_info(capture: FileCapture) -> dict[int, dict[str, str]]:
+    basic_pkt_info: dict[int, dict[str, str | dict[str, str]]] = {}
     counter = 0
     for pkt in capture:
+        counter += 1
         basic_pkt_info[counter] = {
-            "Timestamp": pkt.sniff_timestamp,
-            "Source IP": pkt.ip.src,
-            "Destination IP": pkt.ip.dst,
+            "Timestamp": {
+                "Date": "N/A",
+                "Time": "N/A",
+            },
+            "Source IP": "N/A",
+            "Destination IP": "N/A",
+            "Transport Protocol": "N/A",
             "Source Port": "N/A",
             "Destination Port": "N/A",
+            "Packet Length": "N/A"
         }
-        # Logic for src and dst port has to be different to account for lack of transport layer details
-        counter += 1
+        if has_timestamp(pkt):
+            timestamp = str(pkt.sniff_time)
+            date, time = timestamp.split()
+            basic_pkt_info[counter]["Timestamp"]["Date"] = date
+            basic_pkt_info[counter]["Timestamp"]['Time'] = time
 
-    return basic_pkt_info
+        if has_network_layer(pkt):
+            basic_pkt_info[counter]["Source IP"] = str(pkt.ip.src)
+            basic_pkt_info[counter]["Destination IP"] = str(pkt.ip.dst)
+            basic_pkt_info[counter]["Packet Length"] = str(pkt.ip.len)
 
-print(extract_basic_info(load_pcap("../data/raw/test.pcapng")))
+        if has_transport_layer(pkt):
+            basic_pkt_info[counter]["Transport Protocol"] = str(pkt.transport_layer)
+            basic_pkt_info[counter]["Source Port"] = str(pkt[pkt.transport_layer].srcport)
+            basic_pkt_info[counter]["Destination Port"] = str(pkt[pkt.transport_layer].dstport)
+
+    return dict(basic_pkt_info)
+
+pprint.pprint(extract_basic_info(load_pcap("../data/raw/3-way-handshake.pcap")), sort_dicts=False)
